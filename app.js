ffd4db2ad0b75e159799b04a993c7ccf0351fe87
vipulc8 <vipul.c@simformsolutions.com>
2023-02-13 20:56:23 +0530

    add mutliple digit arithmetic operation

--- app.js
+++ app.js
@@ -45,27 +45,54 @@ for (arith of arithBtn) {
   });
 }
 
+let flag_trig = false;
+
 for (item of btn) {
   item.addEventListener("click", (e) => {
     let btnId = e.target.id;
     switch (btnId) {
-      case "C":
+      case "clear":
         intext.value = "";
         break;
       case "2nd":
         let secBtn1 = "2nd-btn-1";
         let secBtn2 = "d-none";
-        toggleBtn(secondBtn, secBtn1, secBtn2);
+        Array.from(secondBtn).map((btn) => {
+          if (btn.classList.contains(secBtn1)) {
+            btn.classList.toggle(secBtn2);
+          } else {
+            btn.classList.toggle(secBtn2);
+          }
+        });
         break;
       case "second-fn-Trigono":
         let setbtn1 = "trigo-btn-1";
         let setbtn2 = "d-none";
-        toggleBtn(secondTrigo, setbtn1, setbtn2);
+        console.log(flag_trig);
+        Array.from(secondTrigo).map((btn) => {
+          if (btn.classList.contains(setbtn1)) {
+            btn.classList.toggle(setbtn2);
+          } else {
+            btn.classList.toggle(setbtn2);
+          }
+        });
+        console.log(flag_trig);
         break;
       case "hyp":
         let shbtn1 = "trigono-h-btn";
         let shbtn2 = "d-none";
-        toggleBtn(secondTrigoH, shbtn1, shbtn2);
+
+        if (flag_trig == true) {
+          console.log("2nd is one");
+        } else {
+          Array.from(secondTrigoH).map((btn) => {
+            if (btn.classList.contains(shbtn1)) {
+              btn.classList.toggle(shbtn2);
+            } else {
+              btn.classList.toggle(shbtn2);
+            }
+          });
+        }
         break;
       case "pi":
         intext.value += "π";
@@ -112,8 +139,7 @@ for (item of btn) {
         if (intext.value.includes("!")) {
           intext.value = result;
         } else if (intext.value.includes("π")) {
-          let arr = [...intext.value];
-          console.log(arr);
+          intext.value = intext.value.replaceAll("π", "3.14");
         } else if (
           intext.value.includes("^") &&
           !(intext.value.includes("+") && intext.value.includes("-"))
@@ -138,15 +164,15 @@ for (item of btn) {
           intext.value.includes("+") ||
           intext.value.includes("-") ||
           intext.value.includes("*") ||
-          intext.value.includes("/")
+          intext.value.includes("/") ||
+          intext.value.includes("^")
         ) {
-          solution(intext.value);
+          //solution;
+          result = calculate(tokenize(intext.value));
           intext.value = result;
-          result = 0;
         } else if (trigoprator.includes(intext.value.slice(0, 3))) {
           trigohandle(intext.value);
         } else {
-          console.log("last");
           intext.value = result;
         }
         break;
@@ -154,153 +180,208 @@ for (item of btn) {
   });
 }
 
-function solution(expression) {
-  let stackarr = [];
-
-  // Variable topp initialized with -1
-  let topp = -1;
-
-  // Push function for pushing
-  // elements inside stack
-  function push(e) {
-    topp++;
-    stackarr[topp] = e;
-  }
-
-  // Pop function for returning top element
-  function pop() {
-    if (topp == -1) return 0;
-    else {
-      var popped_ele = stackarr[topp];
-      topp--;
-      return popped_ele;
-    }
-  }
-  InfixtoPostfix();
-
-  function InfixtoPostfix() {
-    // Postfix array created
-    let postfix = [];
-    let temp = 0;
-    push("@");
-    let infixval = expression;
-    console.log(infixval);
-    // Iterate on infix string
-    for (let i = 0; i < infixval.length; i++) {
-      let el = infixval[i];
-
-      // Checking whether operator or not
-      if (operator(el)) {
-        if (el == ")") {
-          while (stackarr[topp] != "(") {
-            postfix[temp++] = pop();
-          }
-          pop();
-        }
-
-        // Checking whether el is (  or not
-        else if (el == "(") {
-          push(el);
-        }
-
-        // Comparing precedency of el and
-        // stackarr[topp]
-        else if (precedency(el) > precedency(stackarr[topp])) {
-          push(el);
-        } else {
-          while (precedency(el) <= precedency(stackarr[topp]) && topp > -1) {
-            postfix[temp++] = pop();
-          }
-          push(el);
-        }
+function tokenize(s) {
+  const r = [];
+  let token = "";
+  for (const character of s) {
+    if ("^*/+-".includes(character)) {
+      if (token === "" && character === "-") {
+        token = "-";
       } else {
-        postfix[temp++] = el;
+        r.push(parseFloat(token), character);
+        token = "";
+      }
+    } else {
+      token += character;
+    }
+  }
+  if (token !== "") {
+    r.push(parseFloat(token));
+  }
+  console.log("r", r);
+  return r;
+}
+
+function calculate(tokens) {
+  // --- Perform a calculation expressed as an array of operators and numbers
+  const operatorPrecedence = [
+    { "^": (a, b) => Math.pow(a, b) },
+    { "*": (a, b) => a * b, "/": (a, b) => a / b },
+    { "+": (a, b) => a + b, "-": (a, b) => a - b },
+  ];
+  let operator;
+  for (const operators of operatorPrecedence) {
+    const newTokens = [];
+    for (const token of tokens) {
+      if (token in operators) {
+        operator = operators[token];
+      } else if (operator) {
+        newTokens[newTokens.length - 1] = operator(
+          newTokens[newTokens.length - 1],
+          token
+        );
+        operator = null;
+      } else {
+        newTokens.push(token);
       }
     }
-
-    // Adding character until stackarr[topp] is @
-    while (stackarr[topp] != "@") {
-      postfix[temp++] = pop();
-    }
-
-    let fpostfix = "";
-    let l = postfix.length - 1;
-    for (i = 0; i <= l; i++) {
-      fpostfix += postfix[i];
-    }
-    console.log("postfix expression:", fpostfix);
-    result = postfixEval(fpostfix);
+    tokens = newTokens;
   }
-
-  //postfix to evalution
-
-  function postfixEval(exp) {
-    let stack1 = [];
-
-    // Scan all characters one by one
-    for (let i = 0; i < exp.length; i++) {
-      let c = exp[i];
-
-      // If the scanned character is an operand (number here),
-      // push it to the stack.
-      if (!isNaN(parseInt(c))) stack1.push(c.charCodeAt(0) - "0".charCodeAt(0));
-      //  If the scanned character is an operator, pop two
-      // elements from stack apply the operator
-      else {
-        let val1 = stack1.pop();
-        let val2 = stack1.pop();
-
-        switch (c) {
-          case "+":
-            stack1.push(val2 + val1);
-            break;
-
-          case "-":
-            stack1.push(val2 - val1);
-            break;
-
-          case "/":
-            stack1.push(val2 / val1);
-            break;
-
-          case "*":
-            stack1.push(val2 * val1);
-            break;
-          case "^":
-            stack1.push(val2 ** val1);
-            break;
-        }
-      }
-    }
-    return stack1.pop();
-  }
-  // operator
-  function operator(op) {
-    if (
-      op == "+" ||
-      op == "-" ||
-      op == "^" ||
-      op == "*" ||
-      op == "/" ||
-      op == "(" ||
-      op == ")"
-    ) {
-      return true;
-    } else return false;
-  }
-  //precedency
-  function precedency(pre) {
-    if (pre == "@" || pre == "(" || pre == ")") {
-      return 1;
-    } else if (pre == "+" || pre == "-") {
-      return 2;
-    } else if (pre == "/" || pre == "*") {
-      return 3;
-    } else if (pre == "^") {
-      return 4;
-    } else return 0;
+  if (tokens.length > 1) {
+    console.log("Error: unable to resolve calculation");
+    return tokens;
+  } else {
+    return tokens[0];
   }
 }
+
+// function solution(expression) {
+//   let stackarr = [];
+
+//   // Variable topp initialized with -1
+//   let topp = -1;
+
+//   // Push function for pushing
+//   // elements inside stack
+//   function push(e) {
+//     topp++;
+//     stackarr[topp] = e;
+//   }
+
+//   // Pop function for returning top element
+//   function pop() {
+//     if (topp == -1) return 0;
+//     else {
+//       var popped_ele = stackarr[topp];
+//       topp--;
+//       return popped_ele;
+//     }
+//   }
+//   InfixtoPostfix();
+
+//   function InfixtoPostfix() {
+//     // Postfix array created
+//     let postfix = [];
+//     let temp = 0;
+//     push("@");
+//     let infixval = expression;
+//     console.log(infixval);
+//     // Iterate on infix string
+//     for (let i = 0; i < infixval.length; i++) {
+//       let el = infixval[i];
+
+//       // Checking whether operator or not
+//       if (operator(el)) {
+//         if (el == ")") {
+//           while (stackarr[topp] != "(") {
+//             postfix[temp++] = pop();
+//           }
+//           pop();
+//         }
+
+//         // Checking whether el is (  or not
+//         else if (el == "(") {
+//           push(el);
+//         }
+
+//         // Comparing precedency of el and
+//         // stackarr[topp]
+//         else if (precedency(el) > precedency(stackarr[topp])) {
+//           push(el);
+//         } else {
+//           while (precedency(el) <= precedency(stackarr[topp]) && topp > -1) {
+//             postfix[temp++] = pop();
+//           }
+//           push(el);
+//         }
+//       } else {
+//         postfix[temp++] = el;
+//       }
+//     }
+
+//     // Adding character until stackarr[topp] is @
+//     while (stackarr[topp] != "@") {
+//       postfix[temp++] = pop();
+//     }
+
+//     let fpostfix = "";
+//     let l = postfix.length - 1;
+//     for (i = 0; i <= l; i++) {
+//       fpostfix += postfix[i];
+//     }
+//     console.log("postfix expression:", fpostfix);
+//     result = postfixEval(fpostfix);
+//   }
+
+//   //postfix to evalution
+
+//   function postfixEval(exp) {
+//     let stack1 = [];
+
+//     // Scan all characters one by one
+//     for (let i = 0; i < exp.length; i++) {
+//       let c = exp[i];
+
+//       // If the scanned character is an operand (number here),
+//       // push it to the stack.
+//       if (!isNaN(parseInt(c))) stack1.push(c.charCodeAt(0) - "0".charCodeAt(0));
+//       //  If the scanned character is an operator, pop two
+//       // elements from stack apply the operator
+//       else {
+//         let val1 = stack1.pop();
+//         let val2 = stack1.pop();
+
+//         switch (c) {
+//           case "+":
+//             stack1.push(val2 + val1);
+//             break;
+
+//           case "-":
+//             stack1.push(val2 - val1);
+//             break;
+
+//           case "/":
+//             stack1.push(val2 / val1);
+//             break;
+
+//           case "*":
+//             stack1.push(val2 * val1);
+//             break;
+//           case "^":
+//             stack1.push(val2 ** val1);
+//             break;
+//         }
+//       }
+//     }
+//     return stack1.pop();
+//   }
+//   // operator
+//   function operator(op) {
+//     if (
+//       op == "+" ||
+//       op == "-" ||
+//       op == "^" ||
+//       op == "*" ||
+//       op == "/" ||
+//       op == "(" ||
+//       op == ")"
+//     ) {
+//       return true;
+//     } else return false;
+//   }
+//   //precedency
+//   function precedency(pre) {
+//     if (pre == "@" || pre == "(" || pre == ")") {
+//       return 1;
+//     } else if (pre == "+" || pre == "-") {
+//       return 2;
+//     } else if (pre == "/" || pre == "*") {
+//       return 3;
+//     } else if (pre == "^") {
+//       return 4;
+//     } else return 0;
+//   }
+// }
 
 //Factorial function
 function factorial(newinput) {
@@ -311,18 +392,6 @@ function factorial(newinput) {
     f--;
   }
   return ans;
-}
-
-//toogle btn
-
-function toggleBtn(btnarray, btn1, btn2) {
-  Array.from(btnarray).map((btn) => {
-    if (btn.classList.contains(btn1)) {
-      btn.classList.toggle(btn2);
-    } else {
-      btn.classList.toggle(btn2);
-    }
-  });
 }
 
 //check unit
