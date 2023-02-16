5dce4031bf18e13dadf680736f5bffee1fbce3f4
vipulc8 <vipul.c@simformsolutions.com>
2023-02-16 11:49:12 +0530

    Add rand function and Little bit change the ui

--- app.js
+++ app.js
@@ -210,6 +210,12 @@ for (item of btn) {
         break;
       // Function rand
       case "frand":
+        if (intext.value.length == 0) {
+          intext.value = Math.random();
+        } else {
+          let range = parseInt(intext.value);
+          intext.value = Math.floor(Math.random() * range);
+        }
         break;
       // Function dms
       case "fdms":
@@ -522,154 +528,6 @@ function calculate(tokens) {
   }
 }
 
-// function solution(expression) {
-//   let stackarr = [];
-
-//   // Variable topp initialized with -1
-//   let topp = -1;
-
-//   // Push function for pushing
-//   // elements inside stack
-//   function push(e) {
-//     topp++;
-//     stackarr[topp] = e;
-//   }
-
-//   // Pop function for returning top element
-//   function pop() {
-//     if (topp == -1) return 0;
-//     else {
-//       var popped_ele = stackarr[topp];
-//       topp--;
-//       return popped_ele;
-//     }
-//   }
-//   InfixtoPostfix();
-
-//   function InfixtoPostfix() {
-//     // Postfix array created
-//     let postfix = [];
-//     let temp = 0;
-//     push("@");
-//     let infixval = expression;
-//     console.log(infixval);
-//     // Iterate on infix string
-//     for (let i = 0; i < infixval.length; i++) {
-//       let el = infixval[i];
-
-//       // Checking whether operator or not
-//       if (operator(el)) {
-//         if (el == ")") {
-//           while (stackarr[topp] != "(") {
-//             postfix[temp++] = pop();
-//           }
-//           pop();
-//         }
-
-//         // Checking whether el is (  or not
-//         else if (el == "(") {
-//           push(el);
-//         }
-
-//         // Comparing precedency of el and
-//         // stackarr[topp]
-//         else if (precedency(el) > precedency(stackarr[topp])) {
-//           push(el);
-//         } else {
-//           while (precedency(el) <= precedency(stackarr[topp]) && topp > -1) {
-//             postfix[temp++] = pop();
-//           }
-//           push(el);
-//         }
-//       } else {
-//         postfix[temp++] = el;
-//       }
-//     }
-
-//     // Adding character until stackarr[topp] is @
-//     while (stackarr[topp] != "@") {
-//       postfix[temp++] = pop();
-//     }
-
-//     let fpostfix = "";
-//     let l = postfix.length - 1;
-//     for (i = 0; i <= l; i++) {
-//       fpostfix += postfix[i];
-//     }
-//     console.log("postfix expression:", fpostfix);
-//     result = postfixEval(fpostfix);
-//   }
-
-//   //postfix to evalution
-
-//   function postfixEval(exp) {
-//     let stack1 = [];
-
-//     // Scan all characters one by one
-//     for (let i = 0; i < exp.length; i++) {
-//       let c = exp[i];
-
-//       // If the scanned character is an operand (number here),
-//       // push it to the stack.
-//       if (!isNaN(parseInt(c))) stack1.push(c.charCodeAt(0) - "0".charCodeAt(0));
-//       //  If the scanned character is an operator, pop two
-//       // elements from stack apply the operator
-//       else {
-//         let val1 = stack1.pop();
-//         let val2 = stack1.pop();
-
-//         switch (c) {
-//           case "+":
-//             stack1.push(val2 + val1);
-//             break;
-
-//           case "-":
-//             stack1.push(val2 - val1);
-//             break;
-
-//           case "/":
-//             stack1.push(val2 / val1);
-//             break;
-
-//           case "*":
-//             stack1.push(val2 * val1);
-//             break;
-//           case "^":
-//             stack1.push(val2 ** val1);
-//             break;
-//         }
-//       }
-//     }
-//     return stack1.pop();
-//   }
-//   // operator
-//   function operator(op) {
-//     if (
-//       op == "+" ||
-//       op == "-" ||
-//       op == "^" ||
-//       op == "*" ||
-//       op == "/" ||
-//       op == "(" ||
-//       op == ")"
-//     ) {
-//       return true;
-//     } else return false;
-//   }
-//   //precedency
-//   function precedency(pre) {
-//     if (pre == "@" || pre == "(" || pre == ")") {
-//       return 1;
-//     } else if (pre == "+" || pre == "-") {
-//       return 2;
-//     } else if (pre == "/" || pre == "*") {
-//       return 3;
-//     } else if (pre == "^") {
-//       return 4;
-//     } else return 0;
-//   }
-// }
-
 //Factorial function
 function factorial(newinput) {
   let f = parseInt(newinput),
@@ -891,11 +749,11 @@ function handleCot(text, value) {
       break;
     case "coth":
       console.log("cot h");
-      intext.value = 1 / Math.coth(value);
+      intext.value = 1 / Math.tanh(value);
       break;
     case "cothin":
       console.log("cot h in");
-      intext.value = 1 / Math.acoth(value);
+      intext.value = 1 / Math.atanh(value);
       break;
   }
 }
